1aa3c8cae81871498879bf301063ea7992c6b88e
Randy Zhang (ranzhang) <ranzhang@cisco.com>
2020-04-18 17:35:06 -0500

    update compute

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,7 @@
-# Create a master instance
-resource "google_compute_instance" "master" {
-   name = "${var.env}-master"
+# Create two compute instances
+resource "google_compute_instance" "instances" {
+   count = 2
+   name = "${var.env}-${count.index}"
    machine_type = "custom-1-1024"
    zone = var.zone
    boot_disk {
@@ -17,22 +18,5 @@ resource "google_compute_instance" "master" {
       }
 }
 
-# Create a worker instance
-resource "google_compute_instance" "worker" {
-   name = "${var.env}-worker"
-   machine_type = "custom-1-1024"
-   zone = var.zone
-   boot_disk {
-      initialize_params {
-         image = "ubuntu-1604-lts"
-      }
-   }
-   network_interface {
-      network = "${var.vpc_name}"
-      access_config {}
-   }
-   service_account {
-      scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-      }
-}
+
 
